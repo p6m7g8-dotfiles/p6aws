0fd96abbe3887a80ac869fe421454b3c35df2241
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-03-19 12:18:47 -0400

    chore: chases dependencie renames (#2)

--- lib/util/template.sh
+++ lib/util/template.sh
@@ -13,5 +13,5 @@ p6_aws_template_process() {
     local infile="$1"
     shift 1
 
-    p6_template_process "$P6_DFZ_SRC_P6M7G8_DIR/p6aws/tmpl/$infile" "$@"
+    p6_template_process "$P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6aws/tmpl/$infile" "$@"
 }
