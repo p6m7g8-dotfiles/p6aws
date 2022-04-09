40ac0bf833a6ec2ad518f17aed77e50ef8a8a79b
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-04-09 11:35:16 -0400

    feat: +p6_aws_svc_route53_resources_list(zone)

---
+++ lib/svc/route53/resources.sh
@@ -0,0 +1,19 @@
+# shellcheck shell=bash
+######################################################################
+#<
+#
+# Function: p6_aws_svc_route53_resources_list(zone)
+#
+#  Args:
+#	zone -
+#
+#  Environment:	 RESOURCERECORDS
+#>
+######################################################################
+p6_aws_svc_route53_resources_list() {
+    local zone="$1"
+
+    local hosted_zone_id=$(p6_aws_svc_route53_zones_hosted_id_from_zone "$zone")
+
+    p6_aws_cli_cmd route53 list-resource-record-sets --hosted-zone-id "$hosted_zone_id" --query "'ResourceRecordSets[]'" --output text | sed -e 's,RESOURCERECORDS,\t,'
+}
