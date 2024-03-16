2bdf7e079c5743fa805899d840bc755261647c4b
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-03-16 13:46:06 -0400

    fix: adds missing ''

--- lib/svc/rds/instance.sh
+++ lib/svc/rds/instance.sh
@@ -10,7 +10,7 @@
 ######################################################################
 p6_aws_svc_rds_instance_arns_list() {
 
-    p6_aws_cli_cmd rds describe-db-instances --query "DBInstances[].[DBInstanceArn]" --output text
+    p6_aws_cli_cmd rds describe-db-instances --query "'DBInstances[].[DBInstanceArn]'" --output text
 
     p6_return_void
 }
