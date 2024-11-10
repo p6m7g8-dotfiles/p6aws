15b746cbbf98fb2c3a3ab43223e52d8a7f7ca937
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-11-10 09:00:07 -0500

    feat: +p6_aws_svc_logs_lambda_watch_jq

--- lib/svc/cloudwatch/logs.sh
+++ lib/svc/cloudwatch/logs.sh
@@ -19,28 +19,6 @@ p6_aws_svc_logs_watch() {
 ######################################################################
 #<
 #
-# Function: p6_aws_svc_logs_watch_jq(log_group_name)
-#
-#  Args:
-#	log_group_name -
-#
-#>
-######################################################################
-p6_aws_svc_logs_watch_jq() {
-    local log_group_name="$1"
-
-    p6_aws_svc_logs_watch "$log_group_name" |
-        while read -r line; do
-            json=$(echo "$line" | sed -e 's,^.*_CloudTrail_us-east-1 ,,')
-            echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-            echo "$json" | jq '.'
-            echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-        done
-}
-
-######################################################################
-#<
-#
 # Function: p6_aws_svc_logs_groups_list()
 #
 #>
