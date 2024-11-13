9eaec937c58864f3d078b6b580ded717f8e13532
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-11-12 20:28:27 -0600

    feat: +p6_return_aws_organization_id, +p6_aws_svc_organizations_id_get, +p6_aws_svc_organization_mgmt_account_id_get, +p6_aws_svc_organizations_accounts_list_active (#25)

--- lib/svc/organizations/account.sh
+++ lib/svc/organizations/account.sh
@@ -7,9 +7,39 @@
 ######################################################################
 p6_aws_svc_organizations_accounts_list() {
 
-    p6_aws_cli_cmd organizations list-accounts \
-        --output text \
-        --query "'Accounts[].[Id, Status, JoinedMethod, Arn, Name, Email]'"
+  p6_aws_cli_cmd organizations list-accounts \
+    --output text \
+    --query "'Accounts[].[Id, Status, JoinedMethod, Arn, Name, Email]'"
+}
+
+######################################################################
+#<
+#
+# Function: p6_aws_svc_organizations_accounts_list_active()
+#
+#  Environment:	 ACTIVE
+#>
+######################################################################
+p6_aws_svc_organizations_accounts_list_active() {
+
+  p6_aws_svc_organizations_accounts_list | grep ACTIVE
+}
+
+######################################################################
+#<
+#
+# Function: aws_account_id account_id = p6_aws_svc_organization_mgmt_account_id_get()
+#
+#  Returns:
+#	aws_account_id - account_id
+#
+#>
+######################################################################
+p6_aws_svc_organization_mgmt_account_id_get() {
+
+  local account_id=$(p6_aws_cli_cmd organizations describe-organization --query "Organization.MasterAccountId" --output text)
+
+  p6_return_aws_account_id "$account_id"
 }
 
 ######################################################################
@@ -26,12 +56,12 @@ p6_aws_svc_organizations_accounts_list() {
 #>
 ######################################################################
 p6_aws_svc_organizations_account_id_from_account_name() {
-    local account_name="$1"
+  local account_name="$1"
 
-    account_name=$(p6_string_replace "$account_name" '\"')
+  account_name=$(p6_string_replace "$account_name" '\"')
 
-    local new_account_id
-    new_account_id=$(p6_aws_svc_organizations_accounts_list | awk -v k="$account_name" '$5 ~ k { print $1 }')
+  local new_account_id
+  new_account_id=$(p6_aws_svc_organizations_accounts_list | awk -v k="$account_name" '$5 ~ k { print $1 }')
 
-    p6_return_aws_account_id "$new_account_id"
+  p6_return_aws_account_id "$new_account_id"
 }
