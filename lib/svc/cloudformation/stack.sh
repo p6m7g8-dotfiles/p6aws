7b0ac11fe51dc6ee9814eb7270b3c5c97449ba07
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-11-15 10:04:37 -0600

    feat: +p6_aws_svc_organization_services_enable, +p6_aws_svc_cloudtrail_admin_delegate_register (#27)

--- lib/svc/cloudformation/stack.sh
+++ lib/svc/cloudformation/stack.sh
@@ -1,14 +1,37 @@
 ######################################################################
 #<
 #
-# Function: p6_aws_svc_cloudformation_list()
+# Function: stream  = p6_aws_svc_cloudformation_stacks_list()
+#
+#  Returns:
+#	stream - 
 #
 #>
 ######################################################################
-p6_aws_svc_cloudformation_list() {
+p6_aws_svc_cloudformation_stacks_list() {
 
 	p6_aws_cli_cmd cloudformation describe-stacks \
 		--output text \
 		--query "'Stacks[*].[CreationTime, StackStatus, StackName]'" |
 		sort -k 1,1 -n
+
+	p6_return_stream
+}
+
+######################################################################
+#<
+#
+# Function: p6_aws_cloudformation_stack_delete(stack_name)
+#
+#  Args:
+#	stack_name -
+#
+#>
+######################################################################
+p6_aws_cloudformation_stack_delete() {
+	local stack_name="$1"
+
+	p6_aws_cli_cmd cloudformation delete-stack --stack-name "$stack_name"
+
+	p6_return_void
 }
