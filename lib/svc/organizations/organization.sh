4b8e49cef0515ac78ef99add2bae615b7143690a
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-11-15 07:32:30 -0600

    fix: p6_return_organization_id->p6_return_aws_organization_id (#26)

--- lib/svc/organizations/organization.sh
+++ lib/svc/organizations/organization.sh
@@ -11,5 +11,5 @@ p6_aws_svc_organizations_id_get() {
 
   local organization_id=$(p6_aws_cli_cmd organizations describe-organization --query "Organization.Id" --output text)
 
-  p6_return_organization_id "$organization_id"
+  p6_return_aws_organization_id "$organization_id"
 }
