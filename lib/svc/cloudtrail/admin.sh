7905bacb3745c00e25e4a89e9d81ab4dbf8b8ef2
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-11-16 16:47:05 -0500

    feat: +p6_aws_svc_cloudtrail_trail_logging_start, +p6_aws_svc_cloudtrail_trail_arns

--- lib/svc/cloudtrail/admin.sh
+++ lib/svc/cloudtrail/admin.sh
@@ -10,7 +10,7 @@
 p6_aws_svc_cloudtrail_admin_delegate_register() {
     local account_id="$1"
 
-     p6_aws_cli_cmd cloudtrail register-organization-delegated-admin --member-account-id "$account_id" >/dev/null 2>&1 || true
+    p6_aws_cli_cmd cloudtrail register-organization-delegated-admin --member-account-id "$account_id" || true
 
     p6_return_void
 }
