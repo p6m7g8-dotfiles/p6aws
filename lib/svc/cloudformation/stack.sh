4104e951ef8f53902f2d7b4abee02f620419d66f
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-11-17 07:47:53 -0600

    feat: +p6_aws_svc_organizations_account_name_from_account_id (#32)

--- lib/svc/cloudformation/stack.sh
+++ lib/svc/cloudformation/stack.sh
@@ -12,7 +12,7 @@ p6_aws_svc_cloudformation_stacks_list() {
 
 	p6_aws_cli_cmd cloudformation describe-stacks \
 		--output text \
-		--query "'Stacks[*].[CreationTime, StackStatus, StackName]'" |
+		--query "'Stacks[*].[CreationTime, StackStatus, StackId, StackName]'" |
 		sort -k 1,1 -n
 
 	p6_return_stream
