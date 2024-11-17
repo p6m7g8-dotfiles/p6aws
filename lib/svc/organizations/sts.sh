0619f6000067bf2afe2ab6adf659f4271ed7268d
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-11-17 15:27:00 -0500

    fix: need to save and restore

--- lib/svc/organizations/sts.sh
+++ lib/svc/organizations/sts.sh
@@ -139,6 +139,10 @@ p6_aws_svc_organizations_sts_github_su() {
   local secret_access_key=$(p6_json_eval "$json" "-r" ".Credentials.SecretAccessKey")
   local session_token=$(p6_json_eval "$json" "-r" ".Credentials.SessionToken")
 
+  export PRIOR_AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+  export PRIOR_SECRET_ACCES_KEY=$AWS_SECRET_ACCESS_KEY
+  export PRIOR_SESSION_TOKEN=$AWS_SESSION_TOKEN
+
   export AWS_ACCESS_KEY_ID="$access_key_id"
   export AWS_SECRET_ACCESS_KEY="$secret_access_key"
   export AWS_SESSION_TOKEN="$session_token"
@@ -159,6 +163,14 @@ p6_aws_svc_organizations_sts_github_su_un() {
   unset AWS_ACCESS_KEY_ID
   unset AWS_SECRET_ACCESS_KEY
   unset AWS_SESSION_TOKEN
+
+  export AWS_ACCESS_KEY_ID=$PRIOR_AWS_ACCESS_KEY_ID
+  export AWS_SECRET_ACCESS_KEY=$PRIOR_AWS_SECRET_KEY
+  export AWS_SESSION_TOKEN=$PRIOR_AWS_SESSION_TOKEN
+
+  unset PRIOR_AWS_ACCESS_KEY_ID
+  unset PRIOR_AWS_SECRET_KEY
+  unset PRIOR_AWS_SESSION_TOKEN
 
   p6_return_void
 }
