d9dacfb5e0d165075a2bbdfe31b0b5659e03d4dc
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-11-17 08:47:46 -0500

    feat: +p6_aws_svc_organizations_account_name_from_account_id

--- lib/svc/organizations/account.sh
+++ lib/svc/organizations/account.sh
@@ -28,14 +28,14 @@ p6_aws_svc_organizations_accounts_list_active() {
 ######################################################################
 #<
 #
-# Function: aws_account_id account_id = p6_aws_svc_organization_mgmt_account_id_get()
+# Function: aws_account_id account_id = p6_aws_svc_organization_management_account_id_get()
 #
 #  Returns:
 #	aws_account_id - account_id
 #
 #>
 ######################################################################
-p6_aws_svc_organization_mgmt_account_id_get() {
+p6_aws_svc_organization_management_account_id_get() {
 
   local account_id=$(p6_aws_cli_cmd organizations describe-organization --query "Organization.MasterAccountId" --output text)
 
@@ -64,4 +64,25 @@ p6_aws_svc_organizations_account_id_from_account_name() {
   new_account_id=$(p6_aws_svc_organizations_accounts_list_active | awk -v k="$account_name" '$5 ~ k { print $1 }')
 
   p6_return_aws_account_id "$new_account_id"
+}
+
+######################################################################
+#<
+#
+# Function: str account_name = p6_aws_svc_organizations_account_name_from_account_id(account_id)
+#
+#  Args:
+#	account_id -
+#
+#  Returns:
+#	str - account_name
+#
+#>
+######################################################################
+p6_aws_svc_organizations_account_name_from_account_id() {
+  local account_id="$1"
+
+  local account_name=$(p6_aws_svc_organizations_accounts_list_active | awk -v k="$account_id" '$1 ~ k { print $5 }')
+
+  p6_return_str "$account_name"
 }
