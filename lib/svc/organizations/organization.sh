97fd44311aad96c5d4b4097fb7980cca3832a639
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-11-18 18:02:01 -0500

    feat: +p6_aws_svc_organizations_management_account_id_get

--- lib/svc/organizations/organization.sh
+++ lib/svc/organizations/organization.sh
@@ -13,3 +13,21 @@ p6_aws_svc_organizations_id_get() {
 
   p6_return_aws_organization_id "$organization_id"
 }
+
+######################################################################
+#<
+#
+# Function: aws_account_id master_account_id = p6_aws_svc_organizations_management_account_id_get()
+#
+#  Returns:
+#	aws_account_id - master_account_id
+#
+#>
+######################################################################
+p6_aws_svc_organizations_management_account_id_get() {
+
+  local master_account_id=$(p6_aws_cli_cmd organizations describe-organization --query "Organization.MasterAccountId" --output text)
+
+  p6_return_aws_account_id "$master_account_id" 
+}
+
